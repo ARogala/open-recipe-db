356e3effffc88ef51f9ce6028356c1f1a408ba2b
ARogala <adrogala87@gmail.com>
2019-03-18 12:51:35 -0400

    create initial schema

---
+++ models/recipe.js
@@ -0,0 +1,57 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const RecipeSchema = new Schema({
+	name: {
+		type: String,
+		required: true
+	},
+	//slow cooker
+	category: {
+		type: String,
+		required: true
+	},
+	//meatless
+	subCategory: {
+		type: String,
+		required: true
+	},
+	contributor: {
+		type: String,
+		required: true
+	},
+	date: {
+		type: Date,
+		required: true
+	},
+	//easy medium hard
+	difficulty: {
+		type: String,
+		required: true
+	},
+	prepTime: {
+		type: String,
+		required: true
+	},
+	cookTime: {
+		type: String,
+		required: true
+	},
+	totalTime: {
+		type: String,
+		required: true
+	},
+	ingredients: {
+		type: [String],
+		required: true
+	},
+	instructions: {
+		type: String,
+		required: true
+	},
+	notes: String
+});
+
+const Recipe = mongoose.model('recipe', RecipeSchema);
+
+module.exports = Recipe;
