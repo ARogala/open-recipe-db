5a3bb38f3655d59b9de320b15cf6dce5356b2c23
ARogala <adrogala87@gmail.com>
2019-03-20 10:52:42 -0400

    some front end boiler plate

---
+++ client/src/components/App.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+
+import About from './About';
+
+import logo from '../images/logo.svg';
+
+class App extends Component {
+
+    render() {
+        return (
+            <div className="App">
+                <nav className="nav">
+                    <Link to="/" className="nav__link">
+                        Home
+                    </Link>
+                    <Link to="/about" className="nav__link">
+                        About
+                    </Link>
+                    <Link to="#" className="nav__link">
+                        Random Recipe
+                    </Link>
+                    <Link to="#" className="nav__link">
+                        Create Recipe
+                    </Link>
+                </nav>
+                <header className="header">
+                    <h1>Open Recipe Database</h1>
+                    <h2>Get Cooking</h2>
+                    <img className="header__img" src={logo} alt="logo" />
+                </header>
+                <main role="main">
+                    <Route exact path="/" render={() => <div>HOME</div>} />
+                    <Route path="/about" render={() => <About />} />
+                </main>
+            </div>
+        );
+    }
+}
+
+export default App;
+
+
+
+
